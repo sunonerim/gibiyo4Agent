c79f37cfb45a156deafc52a47c5cb6fbeffbbf67
Sungwon Rim <sunonerim@gmail.com>
2016-06-18 11:38:30 +0900

    1

---
+++ app/src/main/java/kr/ogong/gibiyo/gibiyo4agent/MainActivity.java
@@ -0,0 +1,115 @@
+package kr.ogong.gibiyo.gibiyo4agent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import com.onesignal.OneSignal;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//        webView = (WebView) findViewById(R.id.webView);
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.loadUrl("http://www.google.com");
+
+
+        webview = (WebView)findViewById(R.id.webView);
+        webview.setWebViewClient(new WebClient()); // 응룡프로그램에서 직접 url 처리
+        WebSettings set = webview.getSettings();
+        set.setJavaScriptEnabled(true);
+        set.setBuiltInZoomControls(false);
+
+
+        webview.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.e("AGENT GIBIYO", consoleMessage.message() + '\n' + consoleMessage.messageLevel() + '\n' + consoleMessage.sourceId());
+                return super.onConsoleMessage(consoleMessage);
+            }
+        });
+
+
+        webview.loadUrl("http://gibiyo-ogong.rhcloud.com/agent/app/");
+
+
+
+        // ########################  OneSignal Begin
+        // Logging set to help debug issues, remove before releasing your app.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.WARN);
+
+        OneSignal.startInit(this)
+                .setNotificationOpenedHandler(new ExampleNotificationOpenedHandler())
+                .setAutoPromptLocation(true)
+                .init();
+
+        OneSignal.idsAvailable(new OneSignal.IdsAvailableHandler() {
+            @Override
+            public void idsAvailable(String userId, String registrationId) {
+                String text = "OneSignal UserID:\n" + userId + "\n\n";
+
+                if (registrationId != null)
+                    text = "Google Registration Id:\n" + registrationId + text ;
+                else
+                    text = "Google Registration Id:\nCould not subscribe for push" + text ;
+
+            }
+        });
+
+        // ########################  OneSignal ENd
+
+    }
+
+    class WebClient extends WebViewClient {
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+
+    private class ExampleNotificationOpenedHandler implements OneSignal.NotificationOpenedHandler {
+        /**
+         * Callback to implement in your app to handle when a notification is opened from the Android status bar or
+         * a new one comes in while the app is running.
+         * This method is located in this Application class as an example, you may have any class you wish implement NotificationOpenedHandler and define this method.
+         *
+         * @param message        The message string the user seen/should see in the Android status bar.
+         * @param additionalData The additionalData key value pair section you entered in on onesignal.com.
+         * @param isActive       Was the app in the foreground when the notification was received.
+         */
+        @Override
+        public void notificationOpened(String message, JSONObject additionalData, boolean isActive) {
+            String additionalMessage = "";
+
+            try {
+                if (additionalData != null) {
+                    if (additionalData.has("actionSelected"))
+                        additionalMessage += "Pressed ButtonID: " + additionalData.getString("actionSelected");
+
+                    additionalMessage = message + "\nFull additionalData:\n" + additionalData.toString();
+                }
+
+                Log.d("OneSignalExample", "message:\n" + message + "\nadditionalMessage:\n" + additionalMessage);
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
+    }
+}
